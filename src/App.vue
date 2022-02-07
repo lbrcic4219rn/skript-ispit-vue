b1002a3f86e0a55676bbac8c3bae60def5d5f872
Luka <luka.brcic@tp.rs>
2022-02-07 19:01:24 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+      <b-button>Button</b-button>
+
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
